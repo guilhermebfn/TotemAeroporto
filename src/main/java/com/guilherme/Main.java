85476016b139314c4d3f1ce9ca44870ecd697535
Guilherme Bezerra <guilhermebfneto@gmail.com>
2022-12-02 10:26:14 -0300

    Criação do método imprimirMenu.

--- src/main/java/com/guilherme/Main.java
+++ src/main/java/com/guilherme/Main.java
@@ -1,7 +1,19 @@
 package com.guilherme;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var scanner = new Scanner(System.in);
+
+        imprimirMenu();
+    }
+
+    private static void imprimirMenu() {
+        System.out.println("1 - Comprar uma passagem");
+        System.out.println("2 - Alterar o assento");
+        System.out.println("3 - Alterar titularidade de uma reserva");
+        System.out.println("4 - Cancelar uma reserva");
+        System.out.println("5 - Imprimir lista de passageiros");
     }
 }
